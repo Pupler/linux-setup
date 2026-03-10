641fe658a0893dfa2c6c3ac802afb4badfc143cf
Pupler <misha.reak45@gmail.com>
2026-03-10 12:06:47 +0100

    feat: add package installation with pacman

--- setup.sh
+++ setup.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-# This script setups linux after fresh install
+# This script setups Arch Linux after fresh install
 
 GREEN='\033[0;32m'
 RESET='\033[0m'
 
-if [ "$EUID" -ne 0 ]; then
-    echo "Run with sudo: sudo ./setup.sh"
-    exit 1
-fi
+PACKAGES=(
+    git
+    curl
+    vim
+    htop
+)
 
 print_status() {
     echo -e "${GREEN}==>${RESET} $1"
 }
 
+if [ "$EUID" -ne 0 ]; then
+    print_status "Run with sudo: sudo ./setup.sh"
+    exit 1
+fi
+
 print_status "Hi! The script started."
+
+for package in "${PACKAGES[@]}"; do
+    print_status "Installing ${package}..."
+    pacman -S --noconfirm "${package}"
+done
