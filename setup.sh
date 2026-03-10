c2f3d1eeab6a71256386ea79e027d04e986e606e
Pupler <misha.reak45@gmail.com>
2026-03-10 11:51:22 +0100

    add: root permission check

--- setup.sh
+++ setup.sh
@@ -5,6 +5,11 @@
 GREEN='\033[0;32m'
 RESET='\033[0m'
 
+if [ "$EUID" -ne 0 ]; then
+    echo "Run with sudo: sudo ./setup.sh"
+    exit 1
+fi
+
 print_status() {
     echo -e "${GREEN}==>${RESET} $1"
 }
