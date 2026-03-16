0b5d8ef879a63734a189fbee4d492709aa980788
Pupler <misha.reak45@gmail.com>
2026-03-16 11:01:35 +0100

    feat: add SUCCESS/ERROR/WARNING output functions

--- setup.sh
+++ setup.sh
@@ -3,8 +3,10 @@
 # This script setups Arch Linux after fresh install
 
 GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
 RESET='\033[0m'
-LOG_FILE="setup.log"
+LOG_FILE='setup.log'
 
 PACKAGES=(
     git
@@ -29,9 +31,39 @@ print_status() {
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" >> "${LOG_FILE}"
 }
 
+print_success() {
+    echo -e "${GREEN}[SUCCESS]${RESET} $1"
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] [SUCCESS] $1" >> "${LOG_FILE}"
+}
+
+print_error() {
+    echo -e "${RED}[ERROR]${RESET} $1"
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] [ERROR] $1" >> "${LOG_FILE}"
+}
+
+print_warning() {
+    echo -e "${YELLOW}[WARNING]${RESET} $1"
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] [WARNING] $1" >> "${LOG_FILE}"
+}
+
+install_packages() {
+    for package in "${PACKAGES[@]}"; do
+        if pacman -Q "${package}" &>/dev/null; then
+            print_warning "${package} already installed"
+        else
+            print_status "Installing ${package}..."
+            if pacman -S --noconfirm "${package}"; then
+                print_success "${package} installed"
+            else
+                print_error "Failed to install ${package}"
+            fi
+        fi
+    done
+}
+
 install_yay() {
     if pacman -Q yay &>/dev/null; then
-        print_status "yay already installed"
+        print_warning "yay already installed"
     else
         print_status "Installing yay..."
         pacman -S --noconfirm base-devel git
@@ -47,7 +79,7 @@ install_yay() {
 install_aur_packages() {
     for aur_package in "${AUR_PACKAGES[@]}"; do
         if pacman -Q "${aur_package}" &>/dev/null; then
-            print_status "${aur_package} is already installed"
+            print_warning "${aur_package} is already installed"
         else
             print_status "Installing ${aur_package} from AUR..."
             yay -S --noconfirm "${aur_package}"
@@ -64,15 +96,7 @@ fi
 clear
 print_status "Hi! The script started."
 
-for package in "${PACKAGES[@]}"; do
-    if pacman -Q "${package}" &>/dev/null; then
-        print_status "${package} already installed"
-    else
-        print_status "Installing ${package}..."
-        pacman -S --noconfirm "${package}"
-    fi
-done
-
+install_packages
 install_yay
 install_aur_packages
 
